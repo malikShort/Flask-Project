498c1e2697d0fce2fb581ea29c16bf655c96a162
Gabe-Dombach <gdombach009@gmail.com>
2022-03-30 09:35:18 -0400

    update text

--- Flask/app.py
+++ Flask/app.py
@@ -19,6 +19,38 @@ def connect():
 conn = connect()
 cur = conn.cursor()
 cur.execute('SELECT * FROM str_usrs')
-users = cur.fetchall()
+tempusers = cur.fetchall()
 
+print(tempusers)
+users = []
+for i in tempusers:
+    users.append([i[0],i[1],i[2],i[3],i[4],i[5],i[6]])
 print(users)
+conn.close()
+cur.close()
+
+@app.Route("/login/<error> = ''")
+def loginPath(error = ''):
+    return render_template('login.html',error = error)
+
+@app.Route("/login",methods=['POST', 'GET'])
+def loginFunc():
+    if request.method == 'POST':
+        usr = request.form['nm']
+        upswrd = request.form['pw']
+        for i in users:
+            if i[3] == usr and i[4] == upswrd:
+                print(i[3],i[4])
+                return redirect(url_for('main'),id = i[0])
+            return redirect(url_for('login'),error = 'Incorrect username or password')
+
+    else:
+        usr = request.args.get('nm')
+        uspswrd = request.args.get('pw')
+        for i in users:
+            if i[3] == usr and i[4] == upswrd:
+                print(i[3],i[4])
+                return redirect(url_for('main'),id = i[0])
+            return redirect(url_for('login'),error = 'Incorrect username or password')
+        
+    
