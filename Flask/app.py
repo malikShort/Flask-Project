169c049ac996bcae5a33796fd0f4b1b3551a3216
Gabe-Dombach <gdombach009@gmail.com>
2022-03-31 11:12:42 -0400

    Frontend script done

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
 import psycopg2
-
+userID = int()
 app = Flask(__name__)
-from operator import itemgetter
 
 print('connecting to database')
 
@@ -30,21 +29,21 @@ conn.close()
 cur.close()
 
 @app.route('/main/<id>')
-def main():
-    return render_template('')
+def main(id):
+    return render_template('BicyclePage.html',id = id)
 
-@app.route("/login/<error> = ''")
+@app.route("/login/")
 def loginPath(error = ''):
-    return render_template('login.html',error = error)
-
+    return render_template('Login.html',error = error)
+@app.route("/login/<error>")
+def loginErr(error):
+    return render_template('Login.html',error = error)
 @app.route("/login",methods=['POST', 'GET'])
 def loginFunc():
-
     conn = connect()
     cur = conn.cursor()
     cur.execute('SELECT * FROM str_usrs')
     tempusers = cur.fetchall()
-
     print(tempusers)
     users = []
     for i in tempusers:
@@ -59,8 +58,9 @@ def loginFunc():
         for i in users:
             if i[3] == usr and i[4] == upswrd:
                 print(i[3],i[4])
-                return redirect(url_for('main'),id = i[0])
-            return redirect(url_for('login'),error = 'Incorrect username or password')
+                userID = i[0]
+                return redirect(url_for('main',id = i[0]))
+            return redirect(url_for('login',error = 'Incorrect username or password'))
 
     else:
         usr = request.args.get('nm')
@@ -68,14 +68,20 @@ def loginFunc():
         for i in users:
             if i[3] == usr and i[4] == upswrd:
                 print(i[3],i[4])
-                return redirect(url_for('main'),id = i[0])
-            return redirect(url_for('login'),error = 'Incorrect username or password')
+                userID = i[0]
+                return redirect(url_for('main',id = i[0]))
+            return redirect(url_for('login',error = 'Incorrect username or password'))
         
-@app.route('/register/<error> = ''')
-def register(error):
-    return render_template('RegistrationPage.html',error = error)
 @app.route('/register')
 def register():
+    return render_template('RegistrationPage.html')
+@app.route('/register/<error>')
+
+def registerErr(error):
+    print(error)
+    return render_template('RegistrationPage.html',error = error)
+@app.route('/register',methods=['POST', 'GET'])
+def registering():
 
     conn = connect()
     cur = conn.cursor()
@@ -90,40 +96,115 @@ def register():
     conn.close()
     cur.close()
 
-
     if request.method == 'POST':
         eml = request.form['eml']
         nme = request.form['nme']
         usr = request.form['unR']
-        upswrd = request.form['pw']
+        upswrd = request.form['pwR']
         for i in users:
-            if i[3] == usr and i[4] == upswrd:
+            if i[3] == usr:
                 print(i[3],i[4])
-                return redirect(url_for('register'),error = 'User already Exists')
+                return redirect(url_for('register',error = 'User already Exists'))
 
         conn = connect()
         cur = conn.cursor()
-        cur.execute(f'INSERT INTO str_usrs () (name,email,usernm,password) VALUES ("{nme}","{eml}."{usr}","{upswrd}") ')
+        #cur.execute(f'INSERT INTO str_usrs  (name,email,usernm,password) VALUES ("{nme}","{eml}","{usr}","{upswrd}") ')
+        cur.execute('INSERT INTO str_usrs (name,email,usernm,password)'
+            'VALUES(%s,%s,%s,%s)',
+            (f"{nme}",f"{eml}",f"{usr}",f"{upswrd}"))
         conn.commit()
         cur.close()
         conn.close()
-
-        return redirect(url_for('main'),id = i[0])
+        userID = i[0]
+        return redirect(url_for('main',id = i[0]))
         
 
     else:
         eml = request.args.get('eml')
         nme = request.args.get('nme')
-        usr = request.args.get('nm')
-        upswrd = request.args.get('pw')
+        usr = request.args.get('unR')
+        upswrd = request.args.get('pwR')
         for i in users:
-            if i[3] == usr and i[4] == upswrd:
+            if i[3] == usr:
                 print(i[3],i[4])
-                return redirect(url_for('register'),error = 'User Already Exists')
+                return redirect(url_for('register',error = 'User Already Exists'))
         conn = connect()
         cur = conn.cursor()
-        cur.execute(f'INSERT INTO str_usrs () (name,email,usernm,password) VALUES ("{nme}","{eml}."{usr}","{upswrd}") ')
+        cur.execute(f'INSERT INTO str_usrs  (name,email,usernm,password) VALUES ("{nme}","{eml}","{usr}","{upswrd}") ')
         conn.commit()
+        cur.execute(f'SELECT id FROM str_usrs WHERE name = {nme}')
+        id = cur.fetchall()
+        print(id)
         cur.close()
         conn.close()
         return redirect(url_for('main'),id = i[0])
+
+@app.route('/bike1Code')
+def bike1():
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM bikestock WHERE id = 1')
+    bike = cur.fetchall()
+    bike = bike[0]
+    print(bike[1])
+    cur.execute("""UPDATE str_usrs 
+    SET bcylenmn = %s
+    WHERE id = %s""",(bike[1],userID))
+    conn.commit()
+    cur.execute("UPDATE str_usrs SET bycleprc = %sWHERE id = %s ",(bike[2],userID))
+    conn.commit()
+
+    cur.execute('SELECT * FROM str_usrs')
+    test = cur.fetchall()
+    print(test)
+    
+    cur.close()
+    conn.close()
+    print(bike)
+    print('success1')
+    return 'nothing'
+
+@app.route('/bike2Code')
+def bike2():
+    print('success2')
+    return 'nothing'
+
+@app.route('/bike3Code')
+def bike3():
+    print('success3')
+    return 'nothing'
+
+@app.route('/bike4Code')
+def bike4():
+    print('success4')
+    return 'nothing'
+
+@app.route('/bike5Code')
+def bike5():
+    print('success5')
+    return 'nothing'
+
+@app.route('/bike6Code')
+def bike6():
+    print('success6')
+    return 'nothing'
+
+@app.route('/bike7Code')
+def bike7():
+    print('success7')
+    return 'nothing'
+
+@app.route('/bike8Code')
+def bike8():
+    print('success8')
+    return 'nothing'
+
+@app.route('/bike9Code')
+def bike9():
+    print('success9')
+    return 'nothing'
+
+@app.route('/bike10Code')
+def bike10():
+    print('success20')
+    return 'nothing'
